f7603ad41913261d1c5a6badab5f9d94c6e58b1e
dehadeaaryan <dehadeaaryan@gmail.com>
2024-03-22 10:17:17 -0500

    initial commit

---
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/SuperFrogSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogschedulerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperFrogSchedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperFrogSchedulerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/superfrogschedulerbackend/SuperFrogSchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.superfrogschedulerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperFrogSchedulerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
