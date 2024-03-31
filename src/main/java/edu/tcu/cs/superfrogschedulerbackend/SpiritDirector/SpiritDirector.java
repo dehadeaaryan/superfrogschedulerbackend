ec8a6204cbf19a0256a120b27af5f9c3defa0503
Dave Park <dave.park@tcu.edu>
2024-03-31 15:12:35 -0500

    Format

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirector.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirector.java
@@ -22,46 +22,59 @@ public class SpiritDirector {
     public SpiritDirector() {
     }
 
-    
+
     public Integer getDirectorId() {
         return directorId;
     }
+    
     public void setDirectorId(Integer directorId) {
         this.directorId = directorId;
     }
+
     public String getDirectorFirstName() {
         return directorFirstName;
     }
+
     public void setDirectorFirstName(String directorFirstName) {
         this.directorFirstName = directorFirstName;
     }
+
     public String getDirectorLastName() {
         return directorLastName;
     }
+
     public void setDirectorLastName(String directorLastName) {
         this.directorLastName = directorLastName;
     }
+
     public String getDirectorUser() {
         return directorUser;
     }
+
     public void setDirectorUser(String directorUser) {
         this.directorUser = directorUser;
     }
+
     public String getDirectorPhoneNumber() {
         return directorPhoneNumber;
     }
+
     public void setDirectorPhoneNumber(String directorPhoneNumber) {
         this.directorPhoneNumber = directorPhoneNumber;
     }
+
     public String getPrivileges() {
         return privileges;
     }
+
     public void setPrivileges(String privileges) {
         this.privileges = privileges;
     }
+
     public Boolean getIsLoggedIn() {
         return isLoggedIn;
     }
+
     public void setIsLoggedIn(Boolean isLoggedIn) {
         this.isLoggedIn = isLoggedIn;
     }
