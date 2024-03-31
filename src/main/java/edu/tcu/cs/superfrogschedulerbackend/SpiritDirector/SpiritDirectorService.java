30f0d6815b2ed117a018331c9e08ec3a918ef8ba
Dave Park <dave.park@tcu.edu>
2024-03-31 15:33:46 -0500

    Setup

---
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirectorService.java
@@ -0,0 +1,10 @@
+package edu.tcu.cs.superfrogschedulerbackend.SpiritDirector;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public class SpiritDirectorService extends JpaRepository<SpiritDirector, String>{
+    
+}
