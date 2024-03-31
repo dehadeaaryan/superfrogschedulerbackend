0e51e76f4825f54ee5ae4ac3badf6fc7b271122b
Michael Nguyen <dang.nguyen@tcu.edu>
2024-03-31 16:25:24 -0500

    Create student package

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/student/Student.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/student/Student.java
@@ -1,4 +1,27 @@
 package edu.tcu.cs.superfrogschedulerbackend.student;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private String email;
+    private boolean international;
+
+    private String taxID;
+
+    private String ssn;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName){
+        
+    }
 }
