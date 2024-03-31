c1c52207f30ee62b68f152761db7564c2f8841d8
Dave Park <dave.park@tcu.edu>
2024-03-31 15:01:12 -0500

    Filling out the basics

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirector.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirector.java
@@ -1,5 +1,69 @@
 package edu.tcu.cs.superfrogschedulerbackend.SpiritDirector;
 
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class SpiritDirector {
+    @Id
+    private Integer directorId;
+
+    private String directorFirstName;
+    private String directorLastName;
+    private String directorUser;
+    private String directorPhoneNumber;
+
+    private String privileges;
+    private Boolean isLoggedIn;
+
+    
+
+    public SpiritDirector() {
+    }
+
+    
+    public Integer getDirectorId() {
+        return directorId;
+    }
+    public void setDirectorId(Integer directorId) {
+        this.directorId = directorId;
+    }
+    public String getDirectorFirstName() {
+        return directorFirstName;
+    }
+    public void setDirectorFirstName(String directorFirstName) {
+        this.directorFirstName = directorFirstName;
+    }
+    public String getDirectorLastName() {
+        return directorLastName;
+    }
+    public void setDirectorLastName(String directorLastName) {
+        this.directorLastName = directorLastName;
+    }
+    public String getDirectorUser() {
+        return directorUser;
+    }
+    public void setDirectorUser(String directorUser) {
+        this.directorUser = directorUser;
+    }
+    public String getDirectorPhoneNumber() {
+        return directorPhoneNumber;
+    }
+    public void setDirectorPhoneNumber(String directorPhoneNumber) {
+        this.directorPhoneNumber = directorPhoneNumber;
+    }
+    public String getPrivileges() {
+        return privileges;
+    }
+    public void setPrivileges(String privileges) {
+        this.privileges = privileges;
+    }
+    public Boolean getIsLoggedIn() {
+        return isLoggedIn;
+    }
+    public void setIsLoggedIn(Boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
 
 }
