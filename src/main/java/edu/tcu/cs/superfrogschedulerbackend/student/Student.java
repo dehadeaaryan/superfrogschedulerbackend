ff8e3624adb5fa63c7dc8c7ba76742dd599977ab
Michael Nguyen <dang.nguyen@tcu.edu>
2024-03-31 16:33:23 -0500

    Add setters and getters

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/student/Student.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/student/Student.java
@@ -22,6 +22,58 @@ public class Student implements Serializable {
     }
 
     public void setFirstName(String firstName){
-        
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isInternational() {
+        return international;
+    }
+
+    public void setInternational(boolean international) {
+        this.international = international;
+    }
+
+    public String getTaxID() {
+        return taxID;
+    }
+
+    public void setTaxID(String taxID) {
+        this.taxID = taxID;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
     }
 }
