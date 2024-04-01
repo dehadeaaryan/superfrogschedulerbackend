f52b3e6349b12ad90c29777859851b3c561a2bf1
ofuchia <o.akpom@tcu.edu>
2024-03-31 20:49:13 -0500

    Completed the StudentPayment object with injections to the best of my ability. pom.xml might be missing some dependencies, hence my problem with errors.

---
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/StudentPayment/StudentPaymentService.java
@@ -0,0 +1,18 @@
+package edu.tcu.cs.superfrogschedulerbackend.StudentPayment;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class StudentPaymentService {
+
+    private final StudentPaymentRepository studentPaymentRepository;
+
+    public StudentPaymentService(StudentPaymentRepository studentPaymentRepository) {
+        this.studentPaymentRepository = studentPaymentRepository;
+    }
+
+    public StudentPayment findById(String studentPaymentId){
+        return null;
+    }
+}
