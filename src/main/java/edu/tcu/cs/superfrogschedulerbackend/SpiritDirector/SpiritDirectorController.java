f4c49a876fed4960b74d151a048ebd2e74913a62
Dave Park <dave.park@tcu.edu>
2024-04-08 16:57:17 -0500

    Renamed

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirectorController.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/SpiritDirector/SpiritDirectorController.java
@@ -1,4 +1,4 @@
-package edu.tcu.cs.superfrogschedulerbackend.SpiritDirector;
+package edu.tcu.cs.superfrogschedulerbackend.spiritdirector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
