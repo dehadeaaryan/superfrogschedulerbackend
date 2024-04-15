b150c3a6c0bdc98833bb1e732c3f87040e9b65c5
ofuchia <o.akpom@tcu.edu>
2024-04-15 10:50:17 -0500

    Cleaned up StudentPayment fields

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/studentpayment/StudentPaymentRepository.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/studentpayment/StudentPaymentRepository.java
@@ -1,9 +1,8 @@
 package edu.tcu.cs.superfrogschedulerbackend.studentpayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StudentPaymentRepository extends JpaRepository<StudentPayment, String> {
-    //this is giving me an error. Saying it cant access Cannot access org.springframework.data.repository.ListCrudRepository
 
+public interface StudentPaymentRepository extends JpaRepository<StudentPayment,String> {
 }
