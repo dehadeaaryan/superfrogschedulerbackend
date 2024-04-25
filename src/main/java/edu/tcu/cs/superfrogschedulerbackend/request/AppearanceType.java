aba0cf4f9e066b94df66bc5d2a7eaf907bdceace
ofuchia <o.akpom@tcu.edu>
2024-04-24 22:22:35 -0500

    Enum class added for calculating event prices.

---
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/request/AppearanceType.java
@@ -0,0 +1,22 @@
+package edu.tcu.cs.superfrogschedulerbackend.request;
+
+//this ENUM class will assign the cost of a SuperFrog appearance to the Request based on the type of appearance
+
+public enum AppearanceType {
+
+    //events 14 days prior
+    ONCAMPUS(100),
+    PUBLICNONPROFIT(100),
+    PRIVATERESIDENTIAL(175),
+
+    //events day before a game
+    ONCAMPUSBEFOREGAME(200),
+    PUBLICNONPROFITBEFOREGAME(200),
+    PRIVATERESIDENTIALBEFOREGAME(250);
+
+    final int appearanceCost; //how much it costs for SuperFrog to appear depending on the type of event. Not final cost.
+
+    AppearanceType(int appearanceCost) {
+        this.appearanceCost = appearanceCost;
+    }
+}
