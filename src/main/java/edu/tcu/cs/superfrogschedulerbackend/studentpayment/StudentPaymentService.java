3f122aad3534d866003e982d6d5f6a04111398e2
ofuchia <o.akpom@tcu.edu>
2024-04-29 10:40:28 -0500

    Wrote JPA Query for RequestRepo

--- src/main/java/edu/tcu/cs/superfrogschedulerbackend/studentpayment/StudentPaymentService.java
+++ src/main/java/edu/tcu/cs/superfrogschedulerbackend/studentpayment/StudentPaymentService.java
@@ -5,17 +5,21 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 
 @Service
-//@Transactional  i dont think this is needed.
 public class StudentPaymentService {
 
     private final StudentPaymentRepository studentPaymentRepository;
 
-    private final RequestRepository requestRepository; //getting the SuperFrog appearence request
+    private final RequestRepository requestRepository; //getting the SuperFrog appearence requests
 
     public StudentPaymentService(StudentPaymentRepository studentPaymentRepository, RequestRepository requestRepository) {
         this.studentPaymentRepository = studentPaymentRepository;
         this.requestRepository = requestRepository;
     }
 
-    //create a list of appearances and a list of payment forms
+    //retrieving students from Request
+    //make an empty map
+    //using a for loop, iterate list of requests and put them in according to the students thethered the requests (students as keys)
+    //resulting map will be a key-value pair with unique students and a list of their completed requests
+    //iterate through the list and calculate price
+    //result will be a map with key-value pairs of students and how much to pay them
 }
